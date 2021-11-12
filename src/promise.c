a2099ace920494053515dfcf23fb7d054d58b024
Jason Cairns <jason.cairns@aucklanduni.ac.nz>
2021-11-12 14:18:14 +1300

    fixes from simon for allocation

--- src/promise.c
+++ src/promise.c
@@ -4,8 +4,6 @@
 #include <R.h>
 #include <Rinternals.h>
 // #include <R_ext/eventloop.h>
- 
-int pfd[2];
 
 typedef struct Prms {
     int pfd;
@@ -13,11 +11,14 @@ typedef struct Prms {
     SEXP thenContext; // environment in which to evaluate 'then'
 } Promise;
 
+int pfd[2];
+
 SEXP C_promise(SEXP sExecutor, SEXP sResolve, SEXP rho) {
     pid_t cpid;
     Promise *promise;
     SEXP ptr, R_fcall;
 
+    promise = (Promise *) calloc(1, sizeof(Promise));
     pipe(pfd);
     cpid = fork();
     if (cpid == 0) {
@@ -54,20 +55,22 @@ SEXP run_then(SEXP sPromise) {
     Promise *ppromise;
     char buf[BUFSIZ];
     pid_t cpid;
-    int n;
-    SEXP ans, R_fcall, value;
+    int i, n;
+    SEXP ans, R_fcall, res;
 
     ppromise = R_ExternalPtrAddr(sPromise);
-    while((n = read(ppromise->pfd, buf, sizeof buf)) > 0) {
-        // how to malloc & fill RAWSEXPs?; save as unserialized variable 'value'
-    }
+    i = 0;
+    while((n = read(ppromise->pfd, buf, sizeof buf)) > 0)
+        i += n;
+    res = allocVector(RAWSXP, i);
+    strcpy(buf, res);
     close(ppromise->pfd);
 
     pipe(pfd);
     cpid = fork();
     if (cpid == 0) {
         close(pfd[1]);
-        R_fcall = PROTECT(lang2(ppromise->then, value));
+        R_fcall = PROTECT(lang2(ppromise->then, res));
         ans = PROTECT(eval(R_fcall, ppromise->thenContext));
         C_resolve(ans);
     }
